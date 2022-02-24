a2c19945ed3442820d94955f8567a23e6a5019b0
pk9780 <pkannan9780@gmail.com>
2022-02-24 00:56:24 +0000

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,26 @@
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+messageOne.textContent = "";
 
-messageOne.textContent =''
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+  const location = search.value;
 
-    const location = search.value
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
 
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
-
-})
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
